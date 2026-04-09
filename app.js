905a3331688f79ee83d9c8b7fcae9105379083e9
pasha <pasha@gmail.com>
2026-04-09 11:36:15 +0500

    ADD app.js

--- app.js
+++ app.js
@@ -13,12 +13,11 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
-mongoose.connect(process.env.MONGO_URI, {
-    user: process.env.MONGO_USERNAME,
-    pass: process.env.MONGO_PASSWORD,
+mongoose.connect("mongodb+srv://superuser:SuperPassword@supercluster.d83jj.mongodb.net/superData",
+ {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}, function(err) {
+},  function(err) {
     if (err) {
         console.log("error!! " + err)
     } else {
